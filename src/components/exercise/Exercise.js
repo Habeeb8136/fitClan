05f056e291148739f599d6bb35b0479d10fa2fd3
Habeeb8136 <habeeb041099@gmail.com>
2023-01-11 12:01:13 +0530

    exercise page

--- src/components/exercise/Exercise.js
+++ src/components/exercise/Exercise.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './exercise.css';
 import Plans from'./plans';
+import data from '../../ExerciseData.json'
+import Model, { IExerciseData, IMuscleStats } from 'react-body-highlighter';
+
+
 
 export default function Exercise() {
+  const exersiceData = data;
+  
+  function handleClick(){
+    alert('clicked' )
+  }
+  
   return (
     <div className='exercise'>
       <h3>Your exercise plan is below:</h3>
@@ -10,14 +20,30 @@ export default function Exercise() {
         <h3>Exercise Plans</h3>
         <div className="exercise_plans_container">
           <div className="goal_plan">
-            <Plans />
-            <Plans />
-            <Plans />
-            <Plans />
-            <Plans />
-            <Plans />
+          {exersiceData.map((plan,index)=>{
+            return(
+            <Plans 
+              title={plan.title}
+              details={plan.details}
+              catagory={plan.catagory}
+              bg={plan.backgroudUrl}
+              pdf={plan.pdfUrl}
+            />
+
+            )
+            })}
+
+
+            
           </div>
         </div>
+      </div>
+      <div className='choose-target'>
+        <Model  
+          
+        
+        highlightedColors={["#e65a5a", "#db2f2f"]}
+        />
       </div>
     </div>
   )
