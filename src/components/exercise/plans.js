a8774f24bf3d9320c1c558bc4fba56a52a881dc6
Habeeb8136 <habeeb041099@gmail.com>
2023-01-12 16:30:01 +0530

    plans card

--- src/components/exercise/plans.js
+++ src/components/exercise/plans.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import './plans.css';
 
-export default function plans({title,bg,pdf,details,catagory}) {
+export default function plans(props) {
   
   
 
   return (
-  <div className='exercise__plans' style={{backgroundImage:{bg}}}>
-        <p>{title}</p>
-        <p>{details}</p>
-        <p>{catagory}</p>
-        <a href={pdf}>download plan</a>
+  <div className='exercise__plans' >
+  <img style={{height:"100%",width:"100%"}} src={props.bg} alt=""></img>
+        <p>{props.title} {props.bg}</p>
+        <p>{props.details}</p>
+        <p>{props.catagory}</p>
+        <a href={props.pdf}>download plan</a>
     </div>
   )
 }
